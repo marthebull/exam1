49d95ea37a235ccda79157e9d977d3602b1c1d5f
marthebull <marthebullpettersen@gmail.com>
2022-05-09 14:51:59 +0200

    Update post.js

--- post.js
+++ post.js
@@ -1,16 +1,12 @@
-const queryString = document.location.search;
+const queryString = window.location.search;
+const id = new URLSearchParams(queryString).get('id');
+if (!id) { window.location = "posts.html"; }
 
-const params = new URLSearchParams(queryString);
+let out = document.querySelector("#blog-post");
 
-const post = params.get("post"); 
-console.log(post);
-
-fetch(`https://marthebull.no/exam1/wp-json/wp/v2/posts?${id}`, {
+fetch(`https://marthebull.no/exam1/wp-json/wp/v2/posts/${id}`, {
 	"method": "GET",
-	"headers": {
-		"x-rapidapi-host": "planets-by-api-ninjas.p.rapidapi.com",
-		"x-rapidapi-key": "4585718e2cmshe49b203aadfbfbcp132b5bjsn4f21150ae3e2"
-	}
+
 })
 
 .then(response => response.json())
@@ -22,4 +18,19 @@ fetch(`https://marthebull.no/exam1/wp-json/wp/v2/posts?${id}`, {
 .catch(error => out.innerHTML = "Something's wrong!");
 
 
-
+function listData(amiibo) {
+    console.log(amiibo);
+    document.title = amiibo.character; 
+    let newDiv = `
+        <h1>${title}</h1>
+        <img src="${amiibo.image}" alt="${amiibo.character}">
+        <p>Name: <strong>${amiibo.name}</strong></p>
+        <p>Amiibo Series: ${amiibo.amiiboSeries}</p>
+        <p>Game Series: ${amiibo.gameSeries}</p>
+        <p>Released (in Japan): ${amiibo.release.jp}
+        <p>Type: ${amiibo.type}</p>
+        <p><a href="amiibo.html">Back</a></p>
+    `;
+    out.innerHTML = newDiv;
+    status.innerHTML = "";
+}
