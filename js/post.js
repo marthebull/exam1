dc8dc947295183fdcfd75e04dfb69364ac03507b
marthebull <marthebullpettersen@gmail.com>
2022-05-11 16:54:08 +0200

    Update post.js

--- js/post.js
+++ js/post.js
@@ -20,25 +20,52 @@ const postHero = document.querySelector("#hero-post");
 const output = document.querySelector("#blog-post");
 
 function displayPost (data) {
-    console.log(data); 
-    const title = data.title.rendered;
-    const date = data.date;
-    const category = data.categories;
-    const postContent = data.content.rendered;
-    const heroImg = data.featured_media;
+  console.log(data); 
+  const title = data.title.rendered;
+  const date = data.date;
+  const category = data.categories;
+  const postContent = data.content.rendered;
+  const heroImg = data.featured_media;
+  getImageURL(data.featured_media); 
     
     
 
-    let content = `
-        <h1>${title}</h1>
-        <p class="date">${date}</p>
-        <p>${postContent}</p>  
-    `;
+  let content = `
+      <h1>${title}</h1>
+      <p class="date">${date}</p>
+      <p>${postContent}</p>  
+  `;
 
-    postHero.innerHTML = heroImg;
-    output.innerHTML = content;
-    document.title = title;
+  postHero.innerHTML = heroImg;
+  output.innerHTML = content;
+  document.title = title;
   
+}
+
+
+function getImageURL(id) {
+  fetch(`https://marthebull.no/exam1/wp-json/wp/v2/media${id}`)
+  .then(response => response.json())
+  .then(data => {
+    console.log('Success (getImageURL):', data);
+    addImage (data.source_url);
+  })
+  .catch((error) => {
+    console.error('Error (getImageURL):', error);
+  });
+}
+
+
+
+function addImage(src) {
+  console.log ("IMG: " + src);
+  if (src) {
+    let img = document.createElement("img");
+    img.src = src;
+    img.alt = "";
+    img.width = 640;
+    output.prepend(img);
+  }
 }
 
 
