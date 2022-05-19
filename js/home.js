350d80c8beda5db5f19538c5bf0b2de43974a603
marthebull <marthebullpettersen@gmail.com>
2022-05-19 09:59:20 +0200

    før slider

--- js/home.js
+++ js/home.js
@@ -11,6 +11,8 @@ fetch("https://marthebull.no/exam1/wp-json/wp/v2/posts?_embed&per_page=9&page=1"
 .catch(error => postsCarousel.innerHTML = "Something's wrong!");
 
 const postsCarousel = document.querySelector("#latest-posts");
+const nextBtn = document.querySelector(".next-btn");
+const prevBtn = document.querySelector(".prev-btn");
 
 
 function listData(list){
@@ -29,3 +31,17 @@ function listData(list){
         </div>`;
     }
 }
+
+postsCarousel.forEach((item, i) => {
+    let containerDimensions = item.getBoundingClientRect();
+    let containerWidth = containerDimensions.width;
+    console.log("noe skjer!");
+
+    nextBtn[i].addEventListener('click', () => {
+        item.scrollLeft += containerWidth;
+    })
+
+    prevBtn[i].addEventListener('click', () => {
+        item.scrollLeft -= containerWidth;
+    })
+})
