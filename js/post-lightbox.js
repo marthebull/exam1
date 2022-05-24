55b29b3b511b8e44de30cc93e088ccb3970ca6a5
marthebull <marthebullpettersen@gmail.com>
2022-05-24 15:22:47 +0200

    show date på post

--- js/post-lightbox.js
+++ js/post-lightbox.js
@@ -1,16 +1,13 @@
 
 
-const lightbox = document.createElement("div");
-lightbox.id = 'lightbox';
-document.body.appendChild(lightbox);
 
 
-const images = document.querySelectorAll('img');
-images.forEach(image => {
-  image.addEventListener('click', e => {
-    lightbox.classList.add('active');
-    const img = document.createElement('img');
-    img.scr = image.scr;
-    lightbox.appendChild(img);
-  })
-})
+
+
+
+
+
+
+
+
+
--- js/post.js
+++ js/post.js
@@ -16,23 +16,35 @@ fetch(url)
   console.error('Error:', error);
 });
 
+
+// Henter ut og setter inn innhold til bloggpost
+
 const output = document.querySelector("#blog-post");
 const metaDesc = document.getElementsByTagName('meta');
+//const months = ["01", "02", "03", "04", "05", "06", "07", "08", "09", "10", "11", "12"];
+const letterMonths = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+//const day = ["01", "02", "03", "04", "05", "06", "07", "08", "09", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "23", "24", "25", "26", "27", "28", "29", "30", "31"];
 
 function displayPost (data) {
   console.log(data); 
   const title = data.title.rendered;
-  const date = data.date;
+  const date = new Date(data.date);
   const excerpt = data.excerpt.rendered;
   const postContent = data.content.rendered;
+  const tags = data.tags;
   getImageURL(data.featured_media); 
     
-    
+  let postDate = new Date(data.date);
+  //console.log(eventDate);
+  let yearDate =  postDate.getYear();
+  let monthDate = postDate.getMonth();
+  let dayDate =  postDate.getDate();
 
   let content = `
       <h1>${title}</h1>
-      <p class="date">${date}</p>
-      <p>${postContent}</p>
+      
+      <p class="date">${date.getDate()}. ${letterMonths[monthDate]} ${date.getFullYear()} ${date.getHours()}:${date.getMinutes()}</p>
+      <p class="post-content">${postContent}</p>
   `;
 
   
@@ -70,6 +82,34 @@ function addImage(src) {
 }
 
 
+
+// Lightbox, denne funker på bilde satt inn i html, men når ikke fram til bildene i innleggene lagt inn via content
+
+const lightbox = document.createElement("div");
+lightbox.id = 'lightbox';
+document.body.appendChild(lightbox);
+
+
+// Jeg tror det er her jeg ikke når fram med bare "img" for variabelen "images".
+const images = document.querySelectorAll('img');
+images.forEach(image => {
+  image.addEventListener('click', e => {
+    //console.log(image);
+    lightbox.classList.add('active');
+    const img = document.createElement('img');
+    img.classList.add('active');
+    img.src = image.src;
+    while (lightbox.firstChild) {
+      lightbox.removeChild(lightbox.firstChild);
+    }
+    lightbox.appendChild(img);
+  })
+})
+
+lightbox.addEventListener('click', e => {
+  if (e.target !== e.currentTarget) return
+  lightbox.classList.remove('active');
+})
 
 
 
